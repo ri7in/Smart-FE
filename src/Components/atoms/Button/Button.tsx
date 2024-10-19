01884598861d486f987c06e788d31e801913920c
SAwandya <sachilaawandya@gmail.com>
2024-10-20 00:33:13 +0530

    add api-client

--- src/Components/atoms/Button/Button.tsx
+++ src/Components/atoms/Button/Button.tsx
@@ -12,7 +12,7 @@ const Button: React.FC<ButtonProps> = ({
   ...props
 }) => {
   return (
-    <motion.button
+    <motion.button 
       className={`px-4 py-2 bg-green-600 text-white rounded-md hover:bg-green-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50 ${className}`}
       whileHover={{ scale: 1.05 }}
       whileTap={{ scale: 0.95 }}
